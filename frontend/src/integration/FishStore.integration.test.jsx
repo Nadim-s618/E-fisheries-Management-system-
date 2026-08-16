888510f0c14e4e726b9d73ba8e2dc20ce47b8644
Nadim <Shahoriyernadim@gmail.com>
2026-08-17 00:17:26 +0600

    integration test

--- frontend/src/integration/FishStore.integration.test.jsx
+++ frontend/src/integration/FishStore.integration.test.jsx
@@ -9,6 +9,9 @@ import { fishStoreListings, placedOrder } from '../test/mocks/pageData';
 import { renderWithProviders } from '../test/utils/testUtils';
 
 vi.mock('../lib/api', () => import('../test/mocks/pageApi'));
+vi.mock('../context/useAuth', () => ({
+  useAuth: () => ({ isAuthenticated: false }),
+}));
 
 describe('fish store integration flow', () => {
   beforeEach(() => {
